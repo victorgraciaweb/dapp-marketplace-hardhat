fec0b3fa8dafee3f26aeab79e46092ff197edc38
Víctor Gracia <info@victorgraciaweb.com>
2023-01-22 21:06:14 +0100

    Configurated polygon network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   solidity: "0.8.4",
   paths: {
@@ -12,6 +15,10 @@ module.exports = {
   networks: {
     ganache: {
       url: "http://127.0.0.1:7545"
+    },
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com/v1/9022222eb5b9e65029359f8eff4098beed29aeb1",
+      accounts: [privateKey]
     },
     hardhat: {
     },
